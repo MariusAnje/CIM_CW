c0dad8d4559d3f8684cfc1fa3d525a1dfd3db0b4
MariusAnje <mariusanje@outlook.com>
2023-02-10 15:01:42 -0500

    sync

--- search_SG.py
+++ search_SG.py
@@ -64,22 +64,24 @@ def collect_result_from_file(filename):
     return acc
 
 class SGEval():
-    def __init__(self, th, distance, epochs, attack_runs, eval_runs) -> None:
+    def __init__(self, th, distance, epochs, attack_runs, eval_runs, model, device) -> None:
         self.th = th
         self.distance= distance
         self.epochs = epochs
         self.attack_runs = attack_runs
         self.eval_runs = eval_runs
+        self.model = model
+        self.device = device
 
     def evaluate_dev_var(self, dev_var):
         acc_list = []
         for _ in range(self.eval_runs):
             header = time.time()
             tmp_filename = f"tmp_{header}.txt"
-            os.system(f"python train_multi.py --model QLeNet --noise_type SG --train_var {dev_var} --dev_var {dev_var} --rate_max {self.th} --train_epoch {self.epochs} --verbose True --header {header} > {tmp_filename}")
+            os.system(f"python train_multi.py --model {self.model} --noise_type SG --train_var {dev_var} --dev_var {dev_var} --rate_max {self.th} --train_epoch {self.epochs} --verbose True --header {header} --device {self.device} > {tmp_filename}")
             right = f"{header}_noise_{self.th}_{dev_var}"
             output_filename = f"PGD_result_{header}.txt"
-            os.system(f"python valid_mask.py --model QLeNet --model_path ./ --attack_runs {self.attack_runs} --attack_distance_metric max --attack_dist {self.distance} --drop 0.00 --header {right} --attack_function act > {output_filename}")
+            os.system(f"python valid_mask.py --model {self.model} --model_path ./ --attack_runs {self.attack_runs} --attack_distance_metric max --attack_dist {self.distance} --drop 0.00 --header {right} --attack_function act --device {self.device} > {output_filename}")
             this_acc = collect_result_from_file(output_filename)
             acc_list.append(this_acc)
             os.remove(f"tmp_best_{header}.pt")
@@ -103,7 +105,7 @@ if __name__ == "__main__":
     parser.add_argument('--train_var_end', action='store', type=float, default=None,
             help='device variation [std] before write and verify')
     parser.add_argument('--train_th', action='store', type=float, default=None,
-            help='tolerable accuracy drop')
+            help='threshold for PTGaussian')
     parser.add_argument('--device', action='store', default="cuda:0",
             help='device used')
     parser.add_argument('--model', action='store', default="MLP4", choices=["MLP3", "MLP3_2", "MLP4", "LeNet", "CIFAR", "Res18", "TIN", "QLeNet", "QCIFAR", "QRes18", "QDENSE", "QTIN", "QVGG", "Adv", "QVGGIN", "QResIN"],
@@ -122,7 +124,7 @@ if __name__ == "__main__":
 
     print(args)
 # bounded_search_float(func,1,39,0,10)
-    evaluator = SGEval(args.train_th, args.attack_dist, args.train_epoch, args.attack_runs, args.eval_runs)
+    evaluator = SGEval(args.train_th, args.attack_dist, args.train_epoch, args.attack_runs, args.eval_runs, args.model, args.device)
     # search.evaluate_dev_var(0.2, eval_runs=args.eval_runs)
     searcher = BoundedSearch(args.max_search_iter)
     res = searcher.bounded_search_float(evaluator.evaluate_dev_var, args.train_var_start, args.train_var_end)
