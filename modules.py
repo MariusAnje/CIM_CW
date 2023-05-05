c5e290d7950ba5acb84c2fcc331050bfdb1ccdd9
MariusAnje <mariusanje@outlook.com>
2023-05-05 19:11:13 -0400

    LG

--- modules.py
+++ modules.py
@@ -58,6 +58,8 @@ def set_noise_multiple(self, noise_type, dev_var, rate_max=0, rate_zero=0, write
         set_SU(self, rate_max, dev_var)
     elif noise_type == "SG":
         set_SG(self, rate_max, dev_var)
+    elif noise_type == "LSG":
+        set_LSG(self, rate_max, dev_var)
     elif noise_type == "FSG":
         set_FSG(self, rate_max, rate_zero, dev_var)
     elif noise_type == "CSG":
@@ -69,6 +71,8 @@ def set_noise_multiple(self, noise_type, dev_var, rate_max=0, rate_zero=0, write
     elif noise_type == "FG":
         set_BG(self, rate_max, dev_var)
     elif noise_type == "TG":
+        set_TG(self, rate_max, dev_var)
+    elif noise_type == "LTG":
         set_TG(self, rate_max, dev_var)
     elif noise_type == "ATG":
         set_ATG(self, rate_max, dev_var)
@@ -141,6 +145,13 @@ def set_SG(self, s_rate, dev_var):
     # self.noise[self.noise < -s_rate] = -s_rate
     self.noise = self.noise * scale * dev_var
 
+def set_LSG(self, s_rate, dev_var):
+    scale = self.op.weight.abs().max().item()
+    self.noise = torch.randn_like(self.noise)
+    # self.noise[self.noise > s_rate] = s_rate
+    self.noise[self.noise < -s_rate] = -s_rate
+    self.noise = self.noise * scale * dev_var
+
 def set_FSG(self, s_rate, f_rate, dev_var):
     dev_var = dev_var / np.sqrt((f_rate**2 * 0.4 + 0.6))
     scale = self.op.weight.abs().max().item()
@@ -200,6 +211,12 @@ def set_SL(self, dev_var, s_rate, p_rate=0.1):
 def set_TG(self, s_rate, dev_var):
     scale = self.op.weight.abs().max().item()
     noise = stats.truncnorm.rvs(-np.inf, s_rate, size = self.op.weight.shape)
+    self.noise.data = torch.Tensor(noise).to(self.noise.device).data
+    self.noise = self.noise * scale * dev_var
+
+def set_LTG(self, s_rate, dev_var):
+    scale = self.op.weight.abs().max().item()
+    noise = stats.truncnorm.rvs(-s_rate, np.inf, size = self.op.weight.shape)
     self.noise.data = torch.Tensor(noise).to(self.noise.device).data
     self.noise = self.noise * scale * dev_var
 
